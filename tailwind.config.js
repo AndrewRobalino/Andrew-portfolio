e8dda8bfcd1c1b775722297655878354005c59db
Andrew Robalino Garcia <andrewrobalino1@gmail.com>
2025-11-21 15:50:08 -0500

    Add files via upload
    Initial upload:)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#00ff88"
+      }
+    }
+  },
+  plugins: []
+}
